7fae84d6afdd4358caacc96070c714e9f54008e0
Alexandre Pereira Santos <alex.endrios@hotmail.com>
2018-10-03 16:03:44 -0300

    funcoes #Login - Jekins/Mantis - #Funcionalidade - Mantis - #Em construcao - Jekins

--- features/step_definitions/funcionalidade_mantis.rb
+++ features/step_definitions/funcionalidade_mantis.rb
@@ -1,5 +1,77 @@
-Dado("que estou logado no Sistema Mantis com o usuário {string} e senha {string}") do |string, string2|
+Dado("que estou logado no Sistema Mantis com o usuário {string} e senha {string}") do |user_name, passoword|
+  
+      visit 'http://issues.corp.stefanini.com/login_page.php'
+
+      @login = LoginMantis.new
+      @login.logar user_name, passoword
+      @funcao = FuncionalidadeMantis.new
   end
   
   Quando("eu navego pelos seguintes indicadores") do
+     
+    @funcao.navega_visao
+
+      sleep(5)
+        10.times do
+              @funcao.visao_down
+              sleep(2)
+        end
+
+    @funcao.navega_resumo
+
+      sleep(5)
+        27.times do
+              @funcao.reusumo_down
+              sleep(2)
+       end
+
+    @funcao.navega_status
+    
+      sleep(5)
+      9.times do
+            @funcao.status_down
+            sleep(2)
+      end
+
+      @funcao.navega_prioridade
+    
+      sleep(5)
+      17.times do
+            @funcao.prioridade_down
+            sleep(2)
+      end
+
+      @funcao.navega_seriedade
+    
+      sleep(5)
+      17.times do
+            @funcao.seriedade_down
+            sleep(2)
+      end
+
+      @funcao.navega_categoria
+    
+      sleep(5)
+      9.times do
+            @funcao.categoria_down
+            sleep(2)
+      end
+
+      @funcao.navega_resolucao
+    
+      sleep(5)
+      17.times do
+            @funcao.resolucao_down
+            sleep(2)
+      end
+
+      @funcao.navega_resumo_avancado
+    
+      sleep(5)
+      17.times do
+            @funcao.resumo_avancado_down
+            sleep(2)
+      end
+      
+      @funcao.sair
   end
--- features/support/pages/funcionalidade_mantis_page.rb
+++ features/support/pages/funcionalidade_mantis_page.rb
@@ -1,4 +1,72 @@
 class FuncionalidadeMantis < SitePrism::Page
-    
-   
+   #Mapeando botão
+   def sair
+        click_link('Sair')
+   end 
+   #Método navegar nos paineis
+   def navega_visao
+    click_link('Minha Visão') 
+   end
+
+   def navega_resumo
+        click_link("Resumo")  
+   end
+
+   def navega_status
+        click_link("Por estado")
+   end
+
+   def navega_prioridade
+        click_link('Por seriedade')
+   end
+
+   def navega_seriedade
+        click_link('Por prioridade')
+   end
+
+   def navega_categoria
+        click_link('Por categoria')
+   end
+
+   def navega_resolucao
+        click_link('Por resolução')
+   end
+
+   def navega_resumo_avancado
+        click_link('Resumo Avançado')
+   end
+
+   #Método descer a página
+   def reusumo_down
+        find('body > div:nth-child(5) > span:nth-child(2) > a:nth-child(1)').send_keys(:down)
+   end
+
+   def visao_down
+        find('td.menu:nth-child(1) > a:nth-child(1)').send_keys(:down)
+   end
+
+   def status_down
+        find('body > div:nth-child(6) > span:nth-child(2) > a:nth-child(1)').send_keys(:down)
+   end
+
+   def prioridade_down
+        find('body > div:nth-child(6) > span:nth-child(3) > a:nth-child(1)').send_keys(:down)
+   end
+
+   def seriedade_down
+        find('body > div:nth-child(6) > span:nth-child(3) > a:nth-child(1)').send_keys(:down)
+   end
+
+   def categoria_down
+        find('span.bracket-link:nth-child(5) > a:nth-child(1)').send_keys(:down)
+   end
+
+   def resolucao_down
+        find('span.bracket-link:nth-child(6) > a:nth-child(1)').send_keys(:down)
+   end
+
+   def resumo_avancado_down
+        find('span.bracket-link:nth-child(3) > a:nth-child(1)').send_keys(:down)
+   end
+
 end
